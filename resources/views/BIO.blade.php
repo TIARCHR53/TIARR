229480386d86662a0470ebb6765faf1edfd3cfd8
TIARCHR53 <tiarr790@gmail.com>
2025-07-23 18:27:29 +0700

    first commit

---
+++ resources/views/BIO.blade.php
@@ -0,0 +1,28 @@
+
+</html><!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>biodata</title>
+   <style>
+     body{
+      background:gray;
+     }
+   </style>
+  </head>
+
+  <body >
+
+  <h1>BIODATA<h1>
+
+  <h2>NAMA : TIO ARDIAN <h2>
+  <h2>KELAS : 12 RPL <h2>
+  <h2>SEKOLAH : SMK N 1 TANJUNG PANDAN <h2>
+
+  <div class="img">
+  <img src="aset/p.jpg"  alt=""  height="300px">
+  </div>
+
+  </body>
+</html> 
